31e1c575d78592265f2981ce35f8c4c14d8839b0
alfred2000838 <2000838@sit.singaporetech.edu.sg>
2022-02-28 21:26:21 +0800

    extrack manifest info

--- extrack.js
+++ extrack.js
@@ -1,1 +1,46 @@
-document.body.style.border = "5px solid red";
+var extensionList = document.getElementById('extension-list');
+
+function analyseExtension(e) {
+  //browser.management.setEnabled(e.target.value, true);
+  // parse extension id to find more info
+  //alert(management.ExtensionInfo(e.target.value))
+
+  var table = document.getElementById("myTable");
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var getting = browser.management.get(e.target.value);
+  getting.then(info =>{
+    cell1.innerHTML = info.name;
+    cell2.innerHTML = info.permissions;
+    // info.installType
+    // "admin": the add-on was installed because of an administrative policy.
+    // "development": the add-on was installed unpacked from disk.
+    // "normal": the add-on was installed normally from an install package.
+    // "sideload": the add-on was installed by some other software on the user's computer.
+    // "other": the add-on was installed in some other way.
+
+    // info.version
+    // info.permissions
+    // info.homepageUrl
+  });  
+  document.createElement()
+  e.preventDefault();
+  window.close();
+}
+
+browser.management.getAll().then((extensions) => {
+  for (let extension of extensions) {
+    if (extension.type !== 'extension') {
+      continue;
+    }
+    let option = document.createElement('option');
+    option.textContent = extension.name;
+    option.value = extension.id;
+    if (extension.enabled) {
+      option.selected = true;
+    }
+    extensionList.appendChild(option);
+  }
+});
+extensionList.addEventListener('change', analyseExtension)
