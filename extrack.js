fa48bde2ff9f10a4e701caa5829bd168dbb52fae
suhail97s <suhail1997.ms@gmail.com>
2022-03-14 23:06:40 +0800

    Mutation Observer Tests

--- extrack.js
+++ extrack.js
@@ -1,1 +1,21 @@
 document.body.style.border = "5px solid red";
+
+let observer = new MutationObserver((mutations) => {
+    console.log(mutations)
+    mutations.forEach((mutation) => {
+      let oldValue = mutation.oldValue;
+      let newValue = mutation.target; //.textContent
+      if (oldValue !== newValue) {
+          console.log("The changes are :" + oldValue + " and new value : " + newValue);
+      }
+    });
+  });
+  
+  
+  
+  observer.observe(document.body, {
+    characterDataOldValue: true, 
+    subtree: true, 
+    childList: true, 
+    characterData: true
+  });
