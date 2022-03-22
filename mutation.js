78ca5d6e79681b1decde92240e65e3817bca8540
alfred2000838 <2000838@sit.singaporetech.edu.sg>
2022-03-22 19:58:43 +0800

    dom-compare initial commit

--- mutation.js
+++ mutation.js
@@ -1,52 +1,40 @@
-
-// const observer = new MutationObserver(function (mutations){
-//     mutations.forEach(function(mutation){
-//         if(mutation.addedNodes.length){
-
-//         }
-//     })
-// })
-
-// const targetNode = document.querySelector('ol');
-
-// const config = {
-//   attributes: true, 
-//   childList: true, 
-//   characterData: true
-// };
-  
-// const callback = mutations => {  
-//   mutations.forEach(mutation => {
-//     if (mutation.type === 'childList') {
-//       const listValues = Array.from(targetNode.children)
-//           .map(node => node.innerHTML)
-//           .filter(html => html !== '<br>');
-//       console.log(listValues);
-//     }
-//   });
-// }
-
-// const observer = new MutationObserver(callback);
-
-// observer.observe(targetNode, config);
+// fetch original
+// TODO: how to get active tab URL?
+// TODO: Add button to call this function
 fetch('https://www.w3schools.com/')
   .then(res => res.text())
   .then((responseText) => {
     const doc = new DOMParser().parseFromString(responseText, 'text/html');
     //const h1 = doc.querySelector('h1');
     //console.log(responseText);
-    const original = doc.querySelector('body');
+    const original = doc.querySelector('body');   
 
-    var current = document.getElementsByTagName("body");
-    var bodycontent = current[0];
-
-    // comparison
     console.log(original.innerHTML);
-    console.log(bodycontent.innerHTML);
-    //console.log(doc);
   });
 
-//   const parent = document.querySelector(".parent")
-//   parent.children[0].remove()
 
-  // TO STOP MUTATION CHANGES USE observer.disconnect();
+// fetch current
+var current = document.getElementsByTagName("body");
+var bodycontent = current[0];
+console.log(bodycontent.innerHTML);
+
+var compare = require('dom-compare').compare,
+    reporter = require('dom-compare').GroupingReporter,
+    expected = original.innerHTML, // expected DOM tree
+    actual = bodycontent.innerHTML, // actual one
+    result, diff, groupedDiff;
+
+// compare to DOM trees, get a result object
+result = compare(expected, actual);
+
+// get comparison result
+console.log(result.getResult()); // false cause' trees are different
+
+// get all differences
+diff = result.getDifferences(); // array of diff-objects
+
+// differences, grouped by node XPath
+groupedDiff = reporter.getDifferences(result); // object, key - node XPATH, value - array of differences (strings)
+
+// string representation
+console.log(reporter.report(result));
