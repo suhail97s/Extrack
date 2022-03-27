1d383c26c093ca610de5642fbacf6e89baf0d02b
S692 <2002692@sit.singaporetech.edu.sg>
2022-03-27 16:51:09 +0800

    fixed the comparing issue
    Todo:
    - get the current website dom (not expected, but current)
    - for expected, get the current URL (how?)

--- mutation.js
+++ mutation.js
@@ -1,40 +1,51 @@
 // fetch original
 // TODO: how to get active tab URL?
+var dom =  require('./dom-compare')
+
 // TODO: Add button to call this function
+var original, expected, result, diff, groupedDiff;
 fetch('https://www.w3schools.com/')
   .then(res => res.text())
   .then((responseText) => {
     const doc = new DOMParser().parseFromString(responseText, 'text/html');
     //const h1 = doc.querySelector('h1');
     //console.log(responseText);
-    const original = doc.querySelector('body');   
-
-    console.log(original.innerHTML);
+    original = doc.querySelector('body');   
+    expected = original.innerHTML;
+    // console.log(original.innerHTML);
+    analyseDOM(expected);
   });
 
+function analyseDOM(expected){
+  // fetch current
+  var current = document.getElementsByTagName("body");
+  var bodycontent = current[0];
+  var actual = bodycontent.innerHTML;
 
-// fetch current
-var current = document.getElementsByTagName("body");
-var bodycontent = current[0];
-console.log(bodycontent.innerHTML);
+  console.log("Result:")
+  // compare to DOM trees, get a result object
+  result = dom.compare(expected, actual);
 
-var compare = require('dom-compare').compare,
-    reporter = require('dom-compare').GroupingReporter,
-    expected = original.innerHTML, // expected DOM tree
-    actual = bodycontent.innerHTML, // actual one
-    result, diff, groupedDiff;
+  // get comparison result
+  // console.log("Result:")
+  console.log("Results:" + result.getResult()); // false cause' trees are different
 
-// compare to DOM trees, get a result object
-result = compare(expected, actual);
+  // get all differences
+  diff = result.getDifferences(); // array of diff-objects
 
-// get comparison result
-console.log(result.getResult()); // false cause' trees are different
+  // differences, grouped by node XPath
+  // groupedDiff = reporter.getDifferences(result); // object, key - node XPATH, value - array of differences (strings)
 
-// get all differences
-diff = result.getDifferences(); // array of diff-objects
+  // string representation
+  // console.log(reporter.report(result));
+}
 
-// differences, grouped by node XPath
-groupedDiff = reporter.getDifferences(result); // object, key - node XPATH, value - array of differences (strings)
+  // console.log(bodycontent.innerHTML);
 
-// string representation
-console.log(reporter.report(result));
+  // var dom = require('dom_compare');
+
+  // var expected = original.innerHTML, // expected DOM tree
+  //     actual = bodycontent.innerHTML, // actual one
+  //     result, diff, groupedDiff;
+  // var compare = require('dom-compare').compare,
+  //     reporter = require('dom-compare').GroupingReporter;
