f5ece349fad94fd5c7e6a7fb73c6a10a6dee893f
suhail97s <suhail1997.ms@gmail.com>
2022-04-02 06:16:44 +0800

    added DOM report

--- content-script.js
+++ content-script.js
@@ -8,3 +8,17 @@ browser.runtime.onMessage.addListener(request => {
   // only can parse return value as string, not document type. so changed to innerHTML
   return Promise.resolve({response: currentDOM.innerHTML});
 });
+
+
+// const linkArray = [];
+// const resultsArray = [];
+
+browser.runtime.onMessage.addListener(request => {
+  const {linkArray, resultsArray} = request;
+  // console.log(linkArray);
+  // console.log(resultsArray);
+  // browser.runtime.sendMessage({"linkArray":linkArray, "resultsArray":resultsArray});
+  window.dispatchEvent(new CustomEvent('SendArrays', {links: linkArray, results: resultsArray}));
+  console.log("SENT DISPATCH EVENT TO DOM JS");
+});
+
