a9b79a3d6ef26ec38615bf0a540d17ca45630eef
S692 <2002692@sit.singaporetech.edu.sg>
2022-04-04 22:35:08 +0800

    aESTHETIC and cleanup

--- content-script.js
+++ content-script.js
@@ -2,6 +2,5 @@
 browser.runtime.onMessage.addListener(request => {
   console.log("Message from the background script:");
   let currentDOM = document.querySelector("body");
-  // only can parse return value as string, not document type. so changed to innerHTML
   return Promise.resolve({response: currentDOM.innerHTML});
 });
