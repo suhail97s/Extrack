db806ae2b41c3fea77b32a6f36f0d35e79cf2f5f
suhail97s <suhail1997.ms@gmail.com>
2022-04-04 19:52:30 +0800

    remove duplicates and unnecessary codes

--- dom.js
+++ dom.js
@@ -1,112 +1,44 @@
+/* GET ARRAY RESULTS STORED IN LOCAL STORAGE FROM BUNDLE.JS */
 var linkArray1 = JSON.parse(localStorage.getItem("link_array"));
 var resultsArray2 = JSON.parse(localStorage.getItem("results_array"));
 
-// console.log("THIS DOM JS IS ALREADY LOADAED");
-// // console.log(linkArray);
-
-// // window.runDispatch();
-
-// browser.runtime.onMessage.addListener(message => {
-//     // linkArray = message.linkArray;
-//     // resultsArray = message.resultsArray;
-//     linkArray = message.linkArray;
-//     resultsArray = message.resultsArray;
-    
-//     console.log('DOM HTML');
-//     console.log(linkArray);
-//     console.log(resultsArray);
-// });
-
-// window.addEventListener('SendArrays', function (e) {
-//     linkArray = e.links;
-//     resultsArray = e.results;
-//     console.log('received');
-//     console.log(linkArray);
-//     console.log(resultsArray);
-//   });
-
-// browser.runtime.sendMessage(
-//     {var1:linkArray, var2:resultsArray}
-//  ).then(response => {
-//     /* RESPONSE WE GOT BACK FROM CONTENT-SCRIPT.JS */
-//     // console.log("Message from the content-script.js:");
-//     // console.log(response.response);
-//     // Changing the response back to document type, from currentDOM.innerHTML
-//     linkArray = response.var1;
-//     resultsArray = response.var2;
-//     //get the expected DOM
-   
-//     });
-// browser.runtime.onMessage.addListener(request => {
-//     console.log("Message from the background 2222222222222222222:");
-//     let {linkArray, resultsArray} = request;
-
-//     for (i = 0; i < linkArray.length; i++) {
-//         linkArray1.push(linkArray[i]);
-//       }
-
-//     for (i = 0; i < resultsArray.length; i++) {
-//         resultsArray2.push(resultsArray[i]);
-//       }
-//     test(linkArray1,resultsArray2)
-//     console.log();
-//     console.log(resultsArray2);
-//     console.log("for the love of god, please work");
-//     // browser.runtime.sendMessage({"linkArray":linkArray, "resultsArray":resultsArray});
-//   //   console.log(document.querySelector("body"));
-//   });
-  
-
-// browser.runtime.onMessage.addListener(request => {
-//     console.log("Message to dom js:");
-//     const {linkArray1, resultsArray2} = request;
-//     linkArray = linkArray1;
-//     resultsArray = resultsArray2;
-//     console.log(linkArray);
-//     console.log(resultsArray);
-//     // browser.runtime.sendMessage(linkArray, resultsArray);
-//   });
-console.log("REALEST SHIT");
-
-console.log(linkArray1);
-console.log(resultsArray2);
-
-test(linkArray1,resultsArray2);
-
-function test(linkArray1,resultsArray2){
+var seen = {};
+addTable(linkArray1,resultsArray2);
+function addTable(linkArray1,resultsArray2){
     var table = document.getElementById("table");
     if(linkArray1.length != 0){
-        linkArray1.forEach((links, index)=>{
-        /*=========================== GET HTML ELEMENTS =============================*/
-            let linkName = document.createElement("h5");
-            linkName.innerHTML = links;
-            table.appendChild(linkName);
-            let br = document.createElement("br");
-            let domTable = document.createElement("table");
-            let tbodyDOM = document.createElement("tbody");
-            let trDOMHead = document.createElement("tr");
-            let trDOMBody = document.createElement("tr");
-            let thDOMLevel = document.createElement("th");
-            let tdDOMLevel = document.createElement("td");
-        /*=========================== END OF HTML ELEMENTS =============================*/
-            domTable.className = 'table table-hover table-info';
+        for (var i = 0; i < linkArray1.length; i++) {
+            /*=================== CHECK FOR DUPLICATE LINKS =================== */
+            if (!(linkArray1[i] in seen)) {
+                seen[linkArray1[i]] = true;
+                /*=========================== GET HTML ELEMENTS =============================*/
+                let linkName = document.createElement("h5");
+                linkName.innerHTML = linkArray1[i];
+                table.appendChild(linkName);
+                let br = document.createElement("br");
+                let domTable = document.createElement("table");
+                let tbodyDOM = document.createElement("tbody");
+                let trDOMHead = document.createElement("tr");
+                let trDOMBody = document.createElement("tr");
+                let thDOMLevel = document.createElement("th");
+                let tdDOMLevel = document.createElement("td");
+                /*=========================== END OF HTML ELEMENTS =============================*/
+                domTable.className = 'table table-hover table-info';
             
-            thDOMLevel.innerHTML = "DOM Changes";
-            thDOMLevel.className = "col-lg-2 col-md-2 col-sm-2"
-            trDOMHead.appendChild(thDOMLevel);
-            trDOMHead.className = "table-active"
-            tbodyDOM.appendChild(trDOMHead);
-        
-    
-            tdDOMLevel.innerHTML = resultsArray2[index];
+                thDOMLevel.innerHTML = "DOM Changes";
+                thDOMLevel.className = "col-lg-2 col-md-2 col-sm-2"
+                trDOMHead.appendChild(thDOMLevel);
+                trDOMHead.className = "table-active"
+                tbodyDOM.appendChild(trDOMHead);
             
-        
-            trDOMBody.appendChild(tdDOMLevel);
-            tbodyDOM.appendChild(trDOMBody);
-            domTable.append(tbodyDOM);
-            table.appendChild(domTable);
-            table.appendChild(br);
-    //  }
-            });
+                tdDOMLevel.innerHTML = resultsArray2[i];
+                
+                trDOMBody.appendChild(tdDOMLevel);
+                tbodyDOM.appendChild(trDOMBody);
+                domTable.append(tbodyDOM);
+                table.appendChild(domTable);
+                table.appendChild(br);
+            }
+        }
     }
 }
